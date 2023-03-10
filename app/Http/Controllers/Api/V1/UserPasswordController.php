030ef5651fccab96edbb45cfc223bc8ba701cc79
a-moksh <everything.rabi@gmail.com>
2023-03-10 18:48:38 +0900

    update

--- app/Http/Controllers/Api/V1/UserPasswordController.php
+++ app/Http/Controllers/Api/V1/UserPasswordController.php
@@ -5,18 +5,40 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Models\Api\V1\UserPassword;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Crypt;
 use App\Http\Resources\Api\V1\UserPassword\ListResource;
 
 class UserPasswordController extends Controller
 {
     const PAGE_SIZE_DEFAULT = 30;
 
+    /**
+     * @param Request $request
+     * @param Bool $forRss
+     * 
+     * @return [type]
+     */
     public function index(Request $request, Bool $forRss = false)
     {
         $password = UserPassword::paginate(10);
         ListResource::collection($password);
         return response()->json([
             'password' => $password,
+        ]);
+    }
+
+    /**
+     * @param mixed $id
+     * 
+     * @return [type]
+     */
+    public function decryptPassword($id)
+    {
+        $encryptionData = UserPassword::where('id', $id)->first();
+        return response()->json([
+            'data' => [
+                'password' => Crypt::decryptString($encryptionData->password)
+            ],
         ]);
     }
 }
--- routes/Api/V1/pmanager.api.php
+++ routes/Api/V1/pmanager.api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     Route::get('users', 'Api\V1\UserPasswordController@index');
 });
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('decrypt/{id}', 'Api\V1\UserPasswordController@decryptPassword');
+});
